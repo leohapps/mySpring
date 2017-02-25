af641bf7684472ad83a75b825bdce5ea33d8fa48
leohapps <leohapps@outlook.com>
2017-02-25 21:51:33 +0800

    first blood

---
+++ myJob/src/main/java/com/mvc/leo/myJob/App.java
@@ -0,0 +1,13 @@
+package com.mvc.leo.myJob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
